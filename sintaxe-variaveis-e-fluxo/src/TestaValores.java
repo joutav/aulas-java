de7a46cb7bce484d7b6cdae78b5f3e3eadce038e
joutav <jvo.maga@gmail.com>
2020-07-28 02:34:56 -0300

    Novas aulas
    Novas aulas foram feitas, teste de  variaveis, conversoes e caracteres

---
+++ sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,10 @@
+
+public class TestaValores {
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = 5;
+		System.out.println(segundo);
+		primeiro = 10;
+	}
+}
